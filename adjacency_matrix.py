d2654e6f28f29ab9151a1f4b5435de7205892e69
eal6gf <elum1234@gmail.com>
2021-01-24 13:35:33 -0500

    edited

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -1,25 +1,5 @@
 # a sample adjacency matrix
 
-# this is actually two different graphs lol. you'd have to look through whole thing  otherwise
-# islands problem is just graph traversal with an extra loop to account for each row?
-
-# A sample graph where undirected graph with two connected components.
-# of ABC, DEF. Notice the 6x6 matrix has a 1 in each spot where there's 
-# an edge between the two points.
-#    A B C D E F 
-# A    1 1 
-# B
-# C
-# ... and so on
-# graph = [
-#     [0,1,1,0,0,0],
-#     [1,0,1,0,0,0],
-#     [1,1,0,0,0,0],
-#     [0,0,0,0,1,1],
-#     [0,0,0,1,0,1],
-#     [0,0,0,1,1,0]
-# ]
-
 # A sample graph where undirected graph with two connected components.
 # of ABC-DEF. Notice the 6x6 matrix has a 1 in each spot where there's 
 # an edge between the two points.
@@ -32,19 +12,17 @@ graph = [
     [0,0,0,1,1,0]
 ]
 
-
-
-
-# BFS Traversal in such a graph
+# BFS Traversal in such a graph where the index is basically the node being traversed:
+# {0:A, 1:B, 2:C, ...}
 def BFS(graph):
     # create a queue to explore each child
     queue = [0]
     visited = []
     while len(queue) > 0:
         # mark node as visited
-        print(queue)
 
         index = queue.pop(0)
+        print(index)
         if index in visited:
             continue
         visited.append(index)
